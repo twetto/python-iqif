9daef6ea03dc0873c0256072b19f01d7d80658c4
twetto <franky85@hotmail.com.tw>
2020-07-22 16:21:49 +0800

    Add setup.py

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # python-iqif
 A Python library for the Integer Quadratic Integrate-and-Fire neuron API.
+
+## Dependencies
+
+* python
+
+* [iq-neuron](https://github.com/twetto/iq-neuron)
+
+### makedepends
+
+* python-setuptools
+
+* python-wheel
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-iqif-twetto",
+    version="0.0.1",
+    author="twetto",
+    author_email="franky85912@gmail.com",
+    description="A Python library for the Integer Quadratic Integrate-and-Fire neuron API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/twetto/python-iqif",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
